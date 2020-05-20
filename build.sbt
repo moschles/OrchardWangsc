0a274edd665940601bff5a3da7248cadcbb06bed
mosch ryz <shapkiquarry@gmail.com>
2020-05-20 02:30:15 -0400

    enable vegas plotting functionality

--- build.sbt
+++ build.sbt
@@ -2,9 +2,10 @@ lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.example",
-      scalaVersion := "2.12.4"
+      scalaVersion := "2.11.12"
     )),
-    name := "scalatest-example"
+    name := "OrchardWangsc"
   )
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % Test
+libraryDependencies += "org.vegas-viz" %% "vegas" % "0.3.11"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
-
 import scala.io.Source
 import java.io.{FileNotFoundException, IOException}
 import java.io.File
 import java.io.PrintWriter
-import scala.collection.mutable.Queue
 import scala.annotation.tailrec
+import vegas._
+
 
 object Chatter 
 {
@@ -24,53 +24,31 @@ object Chatter
 	}
 }
 
-object isLetter
-{
-	private val lcase:Set[Char] = "qwertyuiopasdfghjklzxcvbnm".toCharArray.toSet
-	private val ucase:Set[Char] = "QWERTYUIOPASDFGHJKLZXCVBNM".toCharArray.toSet
-	private val setAlpha = lcase union ucase
-	def check( c:Char ):Boolean = setAlpha.contains(c) 
-}
-
-class LetterCaesar( ch:Char )
-{
-	private val lowA = 'A'.toInt
-	private val highZ = 'Z'.toInt
-	val useCh = if( ch.toInt < lowA || ch.toInt > highZ ) 'X' else ch 
-	def +( that:LetterCaesar ):LetterCaesar = {
-		val code = ( (useCh.toInt-lowA) + 
-			(that.useCh.toInt-lowA) ) % 26
-		new LetterCaesar( (code+lowA).toChar ) 
-	}
-	def dig():Char = useCh 
-}
-
 object Main 
 {
-	def fileToChars( fName:String ):List[Char] = {
-		val builder = StringBuilder.newBuilder
-		try {
-			for (line <- Source.fromFile(fName).getLines) {
-				builder.append(line.toString)
-			}			
-		} catch {
-			case e: FileNotFoundException => println(s"File not found $fName ")
-			case e: IOException => println(s"IOException while reading $fName ")
+	@tailrec
+	def tr_countOccurances (co:Char , v:List[Char] , accum:Int ):Int = v match {
+		case Nil => accum
+		case h::tail => {
+			val p = if(co==h) 1  else 0
+			val ppa = p+accum
+			tr_countOccurances(co,tail,ppa)
 		}
-		val bastr = builder.toString()
-		bastr.toCharArray.toList 
 	}
-	
-	def symbolsToString( clist:List[Char] ):String = {
-		val builder = StringBuilder.newBuilder
-		for( el <- clist ){ builder.append(el.toString) }
-		builder.toString()
-	}
-	
-	
-	def countOccurances( fc:Char , clist:List[Char]):(Char,Int) = {
-		val onlyfc = clist.filter(_==fc)
-		(fc, onlyfc.length)
+
+	def countOccurances( co:Char , s:String ):Int = {tr_countOccurances( co , s.toList, 0 )}
+
+	/*
+		Map("country" -> "USA", "population" -> 314),
+						Map("country" -> "UK", "population" -> 64),
+						Map("country" -> "DK", "population" -> 80)
+	* */
+	def makeLittleVegasiMap( Xaxislabel:String ,
+													Xaxiscat:String,
+													Yaxislabel:String,
+													ydata:Int ):Map[String,Any] = {
+			val ret = Map( Xaxislabel->Xaxiscat , Yaxislabel->ydata )
+			ret
 	}
 
 	def main(args: Array[String]): Unit = 
@@ -82,29 +60,60 @@ object Main
 			if( args.length > 0 ) args(0) 
 			else "input.txt" 
 			
-		val outfileName = "output.txt"
+		val outfileName = "output.html"
 		
 		val builder = StringBuilder.newBuilder
 		try {
-			for (line <- Source.fromFile(infileName).getLines) {
+			val txtsource = Source.fromFile(infileName)
+			for (line <- txtsource.getLines) {
 				builder.append(line.toString + ",")
-			}			
+			}
+			txtsource.close()
 		} catch {
 			case e: FileNotFoundException => println(s"File not found $infileName ")
 			case e: IOException => println(s"IOException while reading $infileName ")
 		}
 		
 		if( builder.nonEmpty ) {
+
+			// Count the occurances of lowercase letters in the file.
+			val filecontents = builder.toString()
+			val lowerchars = "abcdefghijklmnopqrstuvwxyz"
+			val chfilter = lowerchars.toSet
+			val filteredcontents = filecontents.filter(  chfilter )
+
+			val tt = lowerchars.toList
+			val lettercounts:List[(Char,Int)] = for(el<-tt)
+				yield {
+					val y:(Char,Int) = (el  , countOccurances(el,filteredcontents))
+					y
+				}
+
+			lettercounts.foreach( dup => println(  s"${dup._1} -> ${dup._2}") )
+
+
+			// Create a plot using vegas.
+			val vegasdata:Seq[Map[String,Any]] = for( el<-lettercounts ) yield {
+				makeLittleVegasiMap("letter" , el._1.toString , "occurance" , el._2  )
+			}
+
+			val occurance_plot = Vegas("Country Pop").
+				withData( vegasdata).encodeX("letter", Nom).encodeY("occurance", Quant).mark(Bar);
+
+			val htmlplot:String = occurance_plot.html.pageHTML("HTMLn")
+
+			// Write the HTML plotter to the output file.
 			val fWriter = new PrintWriter( new File(outfileName) )
-			 
-			try {				
-				fWriter.write( builder.toString ) 
+
+			try {
+				fWriter.write( htmlplot )
 			} catch {
-				case e: IOException => e.printStackTrace
+				case e: FileNotFoundException => println(s"File not found $outfileName ")
+				case e: IOException => println(s"IOException while writing $outfileName ")
 			} finally {
-				fWriter.close() 
+				fWriter.close()
 			}
 		}
-		
 	}
+
 }
